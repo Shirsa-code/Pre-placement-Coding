87e68eeae4d0bee452a73646f5447d747c9a2b1b
Shirsa-code <brishtighosh862@gmail.com>
2025-08-10 20:09:41 +0530

    Create dynamic_array_operations.py

---
+++ Week 2 Assignment/dynamic_array_operations.py
@@ -0,0 +1,16 @@
+#Dynamic Array operation
+
+arr = []
+arr.append(10)
+arr.append(20)
+arr.append(30)
+
+print("After insertion:", arr)
+arr.remove(20) 
+print("After deletion:", arr)
+print("Element at index 1:", arr[1])
+arr.append(40)
+arr.append(50)
+print("After more insertions:", arr)
+arr.pop()
+print("After popping:", arr)
